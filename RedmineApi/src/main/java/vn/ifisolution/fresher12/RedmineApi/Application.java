9c0fbd8666ca4151688a6ea096227e706179750b
khoatran.378 <fresher12@2017>
2017-09-21 11:32:46 +0700

    angay

--- RedmineApi/src/main/java/vn/ifisolution/fresher12/RedmineApi/Application.java
+++
@@ -1,8 +0,0 @@
-package vn.ifisolution.fresher12.RedmineApi;
-
-public class Application {
-	public static void main(String[] args) {
-		System.out.println("__________-");
-	}
-
-}
--- RedmineApi/src/main/java/vn/ifisolution/fresher12/RedmineApi/repository/ProjectEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package vn.ifisolution.fresher12.RedmineApi.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import vn.ifisolution.fresher12.RedmineApi.entityRedmine.ProjectEntity;
-
-public interface ProjectEntityRepository extends ElasticsearchRepository<ProjectEntity, Integer>{
-
-}
