8df4e9e8b60ea4d1858f5565591a54ae1a5fc354
khoatran.378 <fresher12@2017>
2017-09-22 10:58:17 +0700

    connect done with apis.ifisolution.com:8080

---
+++ khoatran.378/Student_CourseCassandra/src/main/java/vn/ifisolution/fresher12/Student_CourseCassandra/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package vn.ifisolution.fresher12.Student_CourseCassandra.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import vn.ifisolution.fresher12.Student_CourseCassandra.model.Student;
+
+public interface StudentRepository extends CrudRepository<Student, String>{
+
+}
